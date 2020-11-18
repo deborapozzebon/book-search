30d80c4e59db56968f0e76a8f5cb1fca21967138
Debora Pozzebon <debora.pozzebon@gmail.com>
2020-11-17 23:02:47 -0300

    loading from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Books from './Books.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      book: '',
+      searchResult: '',
+      apiKey: 'AIzaSyDCD3PxYjfsbMWBrAEEPIx8vnmf_1b0Fpo'
+    }
+
+    this.hadlerChange = this.hadlerChange.bind(this);
+    this.handlerSubmit = this.handlerSubmit.bind(this);
+  }
+
+  hadlerChange(event) {
+    const book = event.target.value;
+    this.setState({ book: book });
+  }
+
+  handlerSubmit(event) {
+    event.preventDefault();
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.book + '&key=' + this.state.apiKey + '&maxResults=10')
+      .then(data => {
+        this.setState({ searchResult: data.data.items });
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Books search</h1>
+        <form onSubmit={this.handlerSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Book name" onChange={this.hadlerChange} />
+          </div>
+          <button type="submit">Search</button>
+        </form>
+        <Books books={this.state.searchResult}/>
+      </div>
+    )
+  }
 }
 
 export default App;
